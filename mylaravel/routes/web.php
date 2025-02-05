7bbd3a094360ccc2232f52aed4cfe1d9e19ad59a
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-02-05 15:10:41 +0700

    user

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\MyController;
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/login',
-    [LoginController::class, 'index']);
+    [LoginController::class, 'login']);
 
 Route::get('/register',
     [RegisterController::class, 'regis']);
@@ -17,6 +18,18 @@ Route::get('/mycontroller/{id?}',
 
 Route::post('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
+
+    Route::post('/register',
+    [RegisterController::class, 'create']);
+
+Route::get('/users' , [UserController::class , 'index']);
+
+Route::get('/user/{id}' , [UserController::class , 'edit']);
+
+Route::put('/user' , [UserController::class , 'edit_action']);
+
+Route::delete('/user' , [UserController::class , 'delete']);
+
 
 Route::get('/', function () {
     return view('home');
