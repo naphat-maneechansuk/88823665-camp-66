df5c9b0e6dd5abad5890465539a67084d213bbd2
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-04-26 15:13:43 +0700

    fix(controller):
    -เเก้ชื่อไฟล์

--- mylaravel/app/Http/Controllers/HomeController.php
+++ mylaravel/app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class homeController extends Controller
+class HomeController extends Controller
 {
     function House(){
         return view('home');
--- mylaravel/app/Http/Controllers/RegisterController.php
+++ mylaravel/app/Http/Controllers/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class registerController extends Controller
+class RegisterController extends Controller
 {
     function regis(){
         return view('register');
